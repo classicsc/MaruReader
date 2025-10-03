5a66eae7acced6e77cb43333eac7eadabd38509e
Sam Smoker <sam@samsmoker.net>
2025-10-03 09:53:03 -0500

    Remove the lookup scheme handler

--- MaruReader/WebKit/DictionaryResultContentView.swift
+++ MaruReader/WebKit/DictionaryResultContentView.swift
@@ -25,10 +25,8 @@ struct DictionaryResultContentView: UIViewRepresentable {
         configuration.userContentController = contentController
         let resourceSchemeHandler = ResourceURLSchemeHandler()
         let mediaSchemeHandler = MediaURLSchemeHandler()
-        let lookupSchemeHandler = DictionaryLookupURLSchemeHandler()
         configuration.setURLSchemeHandler(resourceSchemeHandler, forURLScheme: "marureader-resource")
         configuration.setURLSchemeHandler(mediaSchemeHandler, forURLScheme: "marureader-media")
-        configuration.setURLSchemeHandler(lookupSchemeHandler, forURLScheme: "marureader-lookup")
         let webView = WKWebView(frame: .zero, configuration: configuration)
         webView.isInspectable = true
 
