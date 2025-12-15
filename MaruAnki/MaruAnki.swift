bdf3f4057b6a21e95b7878f71648ec7fd181396e
Sam Smoker <classicsc@users.noreply.github.com>
2025-12-14 18:58:21 -0600

    Add maruanki framework

---
+++ MaruAnki/MaruAnki.swift
@@ -0,0 +1,9 @@
+//
+//  MaruAnki.swift
+//  MaruAnki
+//
+//  Created by Sam Smoker on 12/14/25.
+//
+
+import Foundation
+
---
+++ MaruAnkiTests/MaruAnkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MaruAnkiTests.swift
+//  MaruAnkiTests
+//
+//  Created by Sam Smoker on 12/14/25.
+//
+
+import Testing
+@testable import MaruAnki
+
+struct MaruAnkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
