73b3134a95e9f7eb4281968d0baf868730b3fccd
Sam Smoker <classicsc@users.noreply.github.com>
2026-03-31 19:16:04 -0500

    Add more dictionaries to starter package

--- scripts/run-starterdict.sh
+++ scripts/run-starterdict.sh
@@ -11,8 +11,12 @@ ASSET_OUTPUT_PATH="$ROOT_DIR/build/starterdict.aar"
 
 JITENDEX_URL="https://github.com/stephenmk/stephenmk.github.io/releases/latest/download/jitendex-yomitan.zip"
 KANJI_ALIVE_URL="https://github.com/classicsc/kanji-alive-indexer/releases/latest/download/kanji-alive-mp3-indexed.zip"
+BCCWJ_URL="https://github.com/Kuuuube/yomitan-dictionaries/releases/download/yomitan-permalink/BCCWJ_SUW_LUW_combined.zip"
+WADOKU_URL="https://github.com/classicsc/wadoku-pitch-dictionary-for-yomitan/releases/latest/download/wadoku-pitch.zip"
 JITENDEX_ZIP_PATH="$DOWNLOAD_DIR/jitendex-yomitan.zip"
 KANJI_ALIVE_ZIP_PATH="$DOWNLOAD_DIR/kanji-alive-mp3-indexed.zip"
+BCCWJ_ZIP_PATH="$DOWNLOAD_DIR/BCCWJ_SUW_LUW_combined.zip"
+WADOKU_ZIP_PATH="$DOWNLOAD_DIR/wadoku-pitch.zip"
 SEEDER_BINARY_PATH="$DERIVED_DATA_PATH/Build/Products/Debug/DictionarySeeder"
 
 mkdir -p "$LOG_DIR" "$DOWNLOAD_DIR"
@@ -20,6 +24,8 @@ mkdir -p "$LOG_DIR" "$DOWNLOAD_DIR"
 echo "Downloading starter dictionary archives..."
 curl --fail --location --output "$JITENDEX_ZIP_PATH" "$JITENDEX_URL"
 curl --fail --location --output "$KANJI_ALIVE_ZIP_PATH" "$KANJI_ALIVE_URL"
+curl --fail --location --output "$BCCWJ_ZIP_PATH" "$BCCWJ_URL"
+curl --fail --location --output "$WADOKU_ZIP_PATH" "$WADOKU_URL"
 
 echo "Building DictionarySeeder..."
 "$ROOT_DIR/scripts/run-xcodebuild-with-logs.sh" "build-dictionaryseeder-debug" \
@@ -42,7 +48,7 @@ latest_seed_log_path="$LOG_DIR/latest-starterdict-seed.log"
 echo "Seeding starter dictionary output to $STARTER_OUTPUT_DIR..."
 rm -rf "$STARTER_OUTPUT_DIR"
 set +e
-"$SEEDER_BINARY_PATH" "$STARTER_OUTPUT_DIR" "$JITENDEX_ZIP_PATH" --audio "$KANJI_ALIVE_ZIP_PATH" 2>&1 | tee "$seed_log_path"
+"$SEEDER_BINARY_PATH" "$STARTER_OUTPUT_DIR" "$JITENDEX_ZIP_PATH" "$BCCWJ_ZIP_PATH" "$WADOKU_ZIP_PATH" --audio "$KANJI_ALIVE_ZIP_PATH" 2>&1 | tee "$seed_log_path"
 seed_exit_code=${PIPESTATUS[0]}
 set -e
 
